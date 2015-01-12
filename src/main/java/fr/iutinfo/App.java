082c213140dc962402a68b1ec0ae04d966caaa38
Loic Delwaulle <delwaulle.loic@hotmail.fr>
2015-01-12 17:41:37 +0100

    1er

--- src/main/java/fr/iutinfo/App.java
+++ src/main/java/fr/iutinfo/App.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+//Salut
 @ApplicationPath("/v1/")
 public class App extends Application{
     @Override
