b6cd27cfaaaa9982a3805d673af32329319f030c
Melvin Clavel <melvin.clavel@gmail.com>
2015-01-13 16:17:32 +0100

    Ajout de la classe Utilisateur et methodes ajoutées dans OutilsBDD

--- src/main/java/fr/iutinfo/cargo/OutilBDD.java
+++ src/main/java/fr/iutinfo/cargo/OutilBDD.java
@@ -1,5 +1,6 @@
 package fr.iutinfo.cargo;
 
+import java.util.List;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -20,7 +21,6 @@ public class OutilBDD {
 			e.printStackTrace();
 		}
 	}
-
 	public ArrayList<Trajet> recupererListeTrajets() {
 		ArrayList<Trajet> liste = new ArrayList<Trajet>();
 		ResultSet rs;
@@ -157,7 +157,7 @@ public class OutilBDD {
 			this.close();
 		}
 	}
-
+	
 	public void supprimerTrajet(Trajet t) {
 		try {
 			this.connect();
@@ -170,11 +170,81 @@ public class OutilBDD {
 			this.close();
 		}
 	}
+	
+	public void ajouterUtilisateur(String iduser, String mdp){
+		try {
+			this.connect();
+			String requete = "insert into cargouser(iduser, mdp) values('"+iduser+"','"+mdp+"')s";
+			System.out.println(requete);
+			stmt.executeUpdate(requete);
+			this.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			this.close();
+		}
+	}
+		
+	
+	/*---------------------------------------------------------------------------------------*/
+	
+	//ATTENTION, LES MDPs NE SONT PAS RECUPERES !!
+	public List<String> recupererListeNomUtilisateur(){
+		List<String> liste = new ArrayList<String>();
+		ResultSet rs;
+		try {
+			this.connect();
+			rs = stmt.executeQuery("select iduser from cargouser;");
+			while (rs.next()) {
+				liste.add(rs.getString(1));
 
+			}
+			this.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			this.close();
+		}
+		return liste;
+	}
+	//ATTENTION, LES MDPs NE SONT PAS RECUPERES !!
+	public ArrayList<Utilisateur> recupererListeUtilisateur() {
+		ArrayList<Utilisateur> liste = new ArrayList<Utilisateur>();
+		ResultSet rs;
+		try {
+			this.connect();
+			rs = stmt.executeQuery("select iduser,nom,prenom,numtel,mail from cargouser;");
+			while (rs.next()) {
+				Utilisateur u = new Utilisateur(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4), rs.getString(5));
+				liste.add(u);
+
+			}
+			this.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			this.close();
+		}
+		return (ArrayList<Utilisateur>) liste;
+	}
+
+	public void updateProfil(String iduser,String nom, String prenom, String numtel, String mail){
+		boolean argumentNonNUll = false;
+		String requete = "set ";
+		if(nom !=null){
+			requete+="nom = '"+nom+"'";
+			argumentNonNUll=true;
+		}
+		if(argumentNonNUll){
+			requete+= ",";
+			argumentNonNUll=false;
+		}
+		
+	}
+	
+	/*---------------------------------------------------------------------------------------*/
+	
 	public void creerTables() {
 		try {
 			this.connect();
-			stmt.executeUpdate("CREATE TABLE cargouser(iduser varchar(20) primary key, mdp text);");
+			stmt.executeUpdate("CREATE TABLE cargouser(iduser varchar(20) primary key, mdp text, nom text, prenom text, numtel text, mail text);");
 			stmt.executeUpdate("CREATE TABLE trajet(idtrajet INTEGER PRIMARY KEY AUTOINCREMENT, iduser varchar(20), villedepart text, villearrivee text, datetrajet date, hdep int, harr int, nbplace int, prix float,foreign key (iduser) references cargouser(iduser));");
 			this.close();
 		} catch (SQLException e) {
@@ -182,5 +252,4 @@ public class OutilBDD {
 			this.close();
 		}
 	}
-
 }
---
+++ src/main/java/fr/iutinfo/cargo/Utilisateur.java
@@ -0,0 +1,60 @@
+package fr.iutinfo.cargo;
+
+public class Utilisateur {
+	String iduser;
+	String nom;
+	String prenom;
+	String numtel;
+	String mail;
+	
+	public Utilisateur(String iduser, String nom, String prenom, String numtel, String mail){
+		this.iduser=iduser;
+		this.nom=nom;
+		this.prenom=prenom;
+		this.numtel=numtel;
+		this.mail=mail;
+				
+	}
+
+	public String getIduser() {
+		return iduser;
+	}
+
+	public void setIduser(String iduser) {
+		this.iduser = iduser;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNumtel() {
+		return numtel;
+	}
+
+	public void setNumtel(String numtel) {
+		this.numtel = numtel;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+}
