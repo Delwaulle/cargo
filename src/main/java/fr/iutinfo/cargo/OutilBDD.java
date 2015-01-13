d39cbe00ae0b484e038d85db601baaa48255b8b3
dumazyc <clement.dumazy@gmail.com>
2015-01-13 10:48:16 +0100

    Changement liste trajet en outil BDD

--- src/main/java/fr/iutinfo/cargo/OutilBDD.java
+++ src/main/java/fr/iutinfo/cargo/OutilBDD.java
@@ -4,54 +4,68 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListeTrajet {
-	private List<Trajet> liste = new ArrayList<Trajet>();
+public class OutilBDD {
 	private Connection con;
 	private Statement stmt;
 
-	public ListeTrajet() {
+	public OutilBDD() {	
+
+	}
+	
+	private void connect(){
 		try {
 			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection(
 					"jdbc:postgresql://psqlserv/n3p1", "clavelm", "moi");
 			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from trajet;");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Trajet> recupererListeTrajets() {
+		ArrayList<Trajet> liste = new ArrayList<Trajet>();
+		this.connect();
+		ResultSet rs;
+		try {
+			rs = stmt.executeQuery("select * from trajet;");
 			ResultSetMetaData metadata = rs.getMetaData();
 			while (rs.next()) {
 				for (int i = 1; i < metadata.getColumnCount() + 1; i++) {
 					System.out.println(rs.getString(i));
-					// Importer donnee table !
+					// importer la base
 				}
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		this.close();
+		
+		return (ArrayList<Trajet>) liste;
+	}
+
+	private void close() {
+		try {
+			con.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
-
+		
 	}
 
-	public ArrayList<Trajet> RecupererListeTrajets() {
-
-		return null;
-	}
-
-	public ArrayList<Trajet> RecupererListeTrajets(String iduser,
+	public ArrayList<Trajet> recupererListeTrajets(String iduser,
 			String villeDepart, String villeArrivee, String dateTrajet,
 			int heureDepart, int heureArrivee, double prix) {
 
 		return null;
 	}
 
-	public void AjouterTrajet(Trajet t) {
+	public void ajouterTrajet(Trajet t) {
 
 	}
 	
-	public void SupprimerTrajet(Trajet t) {
+	public void supprimerTrajet(Trajet t) {
 		
 	}
 }
