11c90c73346b2934b7044248446d88cd38f8c535
Melvin Clavel <melvin.clavel@gmail.com>
2015-01-13 16:43:55 +0100

    Commit servlet et formulaire Creer Profil

---
+++ src/main/java/fr/iutinfo/cargo/CreerProfil.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/CreerProfil")
+public class CreerProfil extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html");
+		out.println("<html><head><meta charset=UTF-8>");
+		out.println("<link rel=stylesheet type=text/css href=style.css>");
+		out.println("<title>Création de votre profil</title></head>");
+		out.println("<body>");
+
+		OutilBDD db = new OutilBDD();
+		
+		
+		
+	}
+}
