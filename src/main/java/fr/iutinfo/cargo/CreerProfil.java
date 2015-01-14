0adf72fcf6c8e70099ab470f9146331f34518eaf
Melvin Clavel <melvin.clavel@gmail.com>
2015-01-14 10:03:51 +0100

    Rajout de la dependance dans le pom pour les servlets puis ajout de de CreerProfil

--- src/main/java/fr/iutinfo/cargo/CreerProfil.java
+++ src/main/java/fr/iutinfo/cargo/CreerProfil.java
@@ -21,9 +21,38 @@ public class CreerProfil extends HttpServlet {
 		out.println("<title>Création de votre profil</title></head>");
 		out.println("<body>");
 
-		OutilBDD db = new OutilBDD();
+		out.println("<html>");
+		out.println("<head><meta charset=\"UTF-8\">");
+		out.println("<link rel=stylesheet type=text/css href=style.css>");
+		out.println("<title>Suite de votre profil</title>");
+		out.println("</head>");
+		out.println("<body><center><h1>");
+		out.println("<font color=\"DARKRED\">Créons votre profil</font>");
+		out.println("<FORM METHOD=\"POST\" ACTION=\"servlet/CreerProfil2\">");
+		out.println("Nom :");
+		out.println("<INPUT type=\"text\" value=\"\"name=\"nom\">");
+		out.println("<br/>");
+		out.println("Prénom :");
+		out.println("<INPUT type=\"text\" value=\"\" name=\"prenom\">");
+		out.println("<br/>");
+		out.println("Numero de Telephone");
+		out.println("<INPUT type=\"tel\" value=\"\" name=\"numTel\">");
+		out.println("<br/>");
+		out.println("Adresse Mail");
+		out.println("<INPUT type=\"text\" value=\"\" name=\"mail\">");
+		out.println("<br/>");
+		out.println("<INPUT type=\"hidden\" value="+req.getParameter("login")+" name=\"login\">");
+		out.println("<br/>");
+		out.println("<INPUT type=\"hidden\" value="+req.getParameter("mdp")+" name=\"mdp\">");
+		out.println("<br/>");
+		out.println("<INPUT type=\"submit\" value=\"creer profil\">");
+		out.println("</FORM>");
+		out.println("</center>");
+		out.println("</body></html>");
 		
 		
-		
+
+
+
 	}
 }
