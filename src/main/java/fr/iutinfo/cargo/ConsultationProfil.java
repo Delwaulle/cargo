1b8e223be9793853de8572cd989d426186d1ec99
dumazyc <clement.dumazy@gmail.com>
2015-01-15 16:52:34 +0100

    Ajout dans la consultation du profil d'un lien pour noter un utilisateur

--- src/main/java/fr/iutinfo/cargo/ConsultationProfil.java
+++ src/main/java/fr/iutinfo/cargo/ConsultationProfil.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 
 @SuppressWarnings("serial")
 @WebServlet("/servlet/consultationProfil")
-public class ConsultationProfil extends HttpServlet{
+public class ConsultationProfil extends HttpServlet {
 
 	public void service(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
@@ -25,15 +25,15 @@ public class ConsultationProfil extends HttpServlet{
 		OutilBDD db = new OutilBDD();
 		HttpSession s = req.getSession(true);
 		Utilisateur u = (Utilisateur) s.getAttribute("iduser");
-		String cond=req.getParameter("idcond");
-		String login="";
-		try{
+		String cond = req.getParameter("idcond");
+		String login = "";
+		try {
 			login = u.getIduser();
-		}catch(Exception e){
+		} catch (Exception e) {
 			res.sendRedirect("../login.html");
 		}
-		
-		Utilisateur conducteur=db.recupererUtilisateur(cond);
+
+		Utilisateur conducteur = db.recupererUtilisateur(cond);
 
 		res.setContentType("text/html");
 		out.println("<html><head><meta charset=UTF-8>");
@@ -41,10 +41,24 @@ public class ConsultationProfil extends HttpServlet{
 		out.println("<title>Select</title></head>");
 		out.println("<body><center>");
 		out.println("<h1>" + conducteur.getIduser() + "</h1><br>");
-		out.println("Nom : " + conducteur.getNom()+"<br>");
-		out.println("Prenom : " + conducteur.getPrenom()+"<br>");
-		out.println("Numéro de téléphone : " + conducteur.getNumtel()+"<br>");
-		out.println("Adresse mail : " + conducteur.getMail()+"<br>");
+		out.println("Nom : " + conducteur.getNom() + "<br>");
+		out.println("Prenom : " + conducteur.getPrenom() + "<br>");
+		out.println("Numéro de téléphone : " + conducteur.getNumtel() + "<br>");
+		out.println("Adresse mail : " + conducteur.getMail() + "<br>");
+
+		out.println("<form method=\"post\" action=\"/servlet/ajouterAvisBDD\">");
+		out.println("<div class=input>"
+				+ "<label>Noter ce conducteur :</label>"
+				+ "<select name=nbPlaces>" + "<option value=1>1</option>"
+				+ "<option value=2>2</option>" + "<option value=3>3</option>"
+				+ "<option value=4>4</option>" + "<option value=5>5</option>"
+				+ "</select>" + "</div>");
+		out.println("<p>");
+		out.println("<label for=\"ameliorer\">Comment pensez-vous que je pourrais améliorer mon site ?</label><br />");
+		out.println("<textarea name=\"ameliorer\" id=\"ameliorer\"></textarea>");
+		out.println("</p>");
+		out.println("<INPUT type = \"submit\" value = \"Noter\"/>");
+		out.println("</form>");
 		out.println("</center></body><html>");
 
 	}
