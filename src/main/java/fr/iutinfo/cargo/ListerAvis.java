f122a5998984c634083487834e8a0a4cd000678c
Loic Delwaulle <delwaulle.loic@hotmail.fr>
2015-01-15 15:06:30 +0100

    avis

--- src/main/java/fr/iutinfo/cargo/ListerAvis.java
+++ src/main/java/fr/iutinfo/cargo/ListerAvis.java
@@ -26,7 +26,7 @@ public class ListerAvis  extends HttpServlet{
 			res.sendRedirect("../login.html");
 		}
 		OutilBDD obdd=new OutilBDD();
-		//ArrayList<String>  avis=obdd.recupererAvis(login);
+		ArrayList<Avis>  avis=obdd.recupererAvis(login);
 		ArrayList<String> avisTronc=new ArrayList<String>();
 		out.println("<html>");
 		out.println("<head><link href=../bootstrap/css/bootstrap.min.css type=text/css rel=stylesheet> <link href=../avis.css type=text/css rel=stylesheet>  <Title> Avis </Title></head>");
@@ -34,25 +34,38 @@ public class ListerAvis  extends HttpServlet{
 		out.println("<table class=table table-striped><thead><tr>");
 		out.println("<th>#</th>");
 		out.println("<th>Conducteur</th>");
+		out.println("<th>Note</th>");
 		out.println("<th>Avis</th>");
+		out.println("<th>Auteur</th>");
 		out.println("</tr></thead>");
 		
 		out.println("<tbody class=zebra-striped>");
-		/*for(int i=0;i<avis.size();i++){
-			for(int j=0;j<avis.get(i).length();j++){
+		for(int i=0;i<avis.size();i++){
+			for(int j=0;j<avis.get(i).getAvis().length();j++){
 				if(j%20==0){
-					avisTronc.add("<br>"+avis.get(i));
+					avisTronc.add("<br>"+avis.get(i).getAvis());
 				}else 
-					avisTronc.add(avis.get(i));
+					avisTronc.add(avis.get(i).getAvis());
 			}
-		}*/
-		for(int i=0;i<4;i++){
+		}
+		for(int i=0;i<avis.size();i++){
 			out.println("<tr>");
 			out.println("<td>"+i+"</td>");
-			out.println("<td>"+login+"</td>");
-			out.println("<td>"+"looo0000000000"+"<br>"+"loooool"+"</td>");
+			out.println("<td>"+avis.get(i).getConducteur()+"</td>");
+			out.println("<td>"+avis.get(i).getNote()+"</td>");
+			out.println("<td>"+avisTronc.get(i)+"</td>");
+			out.println("<td>"+avis.get(i).getPassager()+"</td>");
 			out.println("</tr>");
 		}
+		
+		/*
+		 * 
+		 * this.idavis = idavis;
+		this.conducteur = conducteur;
+		this.passager = passager;
+		this.avis = avis;
+		this.note = note;
+		 */
 		out.println("</tbody>");
 		out.println("</body>");
 		out.println("</table>");
